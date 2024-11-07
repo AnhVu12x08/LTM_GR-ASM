b604bd4ef8af3b71a2ad8e48e66d9eca28ad2007
AnhVu12x08 <truonganhvu120804@gmail.com>
2024-11-08 01:46:19 +0700

    init project

---
+++ Chat-app Client/Signin.Designer.cs
@@ -0,0 +1,337 @@
+﻿namespace Chat_app_Client
+{
+    partial class Signin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnStart = new Button();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            label6 = new Label();
+            label1 = new Label();
+            btnSignin = new Button();
+            lblSignin = new Label();
+            label7 = new Label();
+            txtSigninPassword = new TextBox();
+            label8 = new Label();
+            txtSigninUsername = new TextBox();
+            label9 = new Label();
+            label10 = new Label();
+            label11 = new Label();
+            txtSigninPhoneNumber = new TextBox();
+            label12 = new Label();
+            txtSigninPasswordConfirm = new TextBox();
+            label13 = new Label();
+            textBox4 = new TextBox();
+            SuspendLayout();
+            // 
+            // btnStart
+            // 
+            btnStart.BackColor = Color.RosyBrown;
+            btnStart.FlatStyle = FlatStyle.Flat;
+            btnStart.Font = new Font("Times New Roman", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btnStart.ForeColor = Color.Maroon;
+            btnStart.Location = new Point(28, 560);
+            btnStart.Margin = new Padding(3, 4, 3, 4);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(370, 48);
+            btnStart.TabIndex = 14;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 15.75F, FontStyle.Italic, GraphicsUnit.Point);
+            label4.Location = new Point(28, 329);
+            label4.Name = "label4";
+            label4.Size = new Size(121, 31);
+            label4.TabIndex = 12;
+            label4.Text = "Password";
+            // 
+            // textBox2
+            // 
+            textBox2.Enabled = false;
+            textBox2.Font = new Font("Times New Roman", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(28, 369);
+            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(370, 42);
+            textBox2.TabIndex = 4;
+            textBox2.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 15.75F, FontStyle.Italic, GraphicsUnit.Point);
+            label3.Location = new Point(28, 223);
+            label3.Name = "label3";
+            label3.Size = new Size(128, 31);
+            label3.TabIndex = 13;
+            label3.Text = "Username";
+            // 
+            // textBox1
+            // 
+            textBox1.Enabled = false;
+            textBox1.Font = new Font("Times New Roman", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(28, 263);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(370, 42);
+            textBox1.TabIndex = 2;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Times New Roman", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label6.ForeColor = Color.Maroon;
+            label6.Location = new Point(160, 77);
+            label6.Name = "label6";
+            label6.Size = new Size(109, 39);
+            label6.TabIndex = 9;
+            label6.Text = "Signin";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 27.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.ForeColor = Color.Maroon;
+            label1.Location = new Point(53, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(341, 52);
+            label1.TabIndex = 10;
+            label1.Text = "Server Chat-app";
+            // 
+            // btnSignin
+            // 
+            btnSignin.BackColor = Color.RosyBrown;
+            btnSignin.FlatStyle = FlatStyle.Flat;
+            btnSignin.Font = new Font("Times New Roman", 21.75F, FontStyle.Bold, GraphicsUnit.Point);
+            btnSignin.ForeColor = Color.Maroon;
+            btnSignin.Location = new Point(28, 554);
+            btnSignin.Margin = new Padding(3, 4, 3, 4);
+            btnSignin.Name = "btnSignin";
+            btnSignin.Size = new Size(370, 61);
+            btnSignin.TabIndex = 4;
+            btnSignin.Text = "Signin";
+            btnSignin.UseVisualStyleBackColor = false;
+            btnSignin.Click += btnSignin_Click;
+            // 
+            // lblSignin
+            // 
+            lblSignin.AutoSize = true;
+            lblSignin.Cursor = Cursors.Hand;
+            lblSignin.Font = new Font("Times New Roman", 14.25F, FontStyle.Italic | FontStyle.Underline, GraphicsUnit.Point);
+            lblSignin.Location = new Point(84, 640);
+            lblSignin.Name = "lblSignin";
+            lblSignin.Size = new Size(265, 27);
+            lblSignin.TabIndex = 5;
+            lblSignin.Text = "Already have an account?";
+            lblSignin.Click += lblSignin_Click;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Times New Roman", 15.75F, FontStyle.Italic, GraphicsUnit.Point);
+            label7.Location = new Point(28, 329);
+            label7.Name = "label7";
+            label7.Size = new Size(121, 31);
+            label7.TabIndex = 22;
+            label7.Text = "Password";
+            // 
+            // txtSigninPassword
+            // 
+            txtSigninPassword.Font = new Font("Times New Roman", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            txtSigninPassword.Location = new Point(28, 369);
+            txtSigninPassword.Margin = new Padding(3, 4, 3, 4);
+            txtSigninPassword.Name = "txtSigninPassword";
+            txtSigninPassword.Size = new Size(370, 42);
+            txtSigninPassword.TabIndex = 3;
+            txtSigninPassword.UseSystemPasswordChar = true;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Times New Roman", 15.75F, FontStyle.Italic, GraphicsUnit.Point);
+            label8.Location = new Point(28, 223);
+            label8.Name = "label8";
+            label8.Size = new Size(128, 31);
+            label8.TabIndex = 23;
+            label8.Text = "Username";
+            // 
+            // txtSigninUsername
+            // 
+            txtSigninUsername.Font = new Font("Times New Roman", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            txtSigninUsername.Location = new Point(28, 263);
+            txtSigninUsername.Margin = new Padding(3, 4, 3, 4);
+            txtSigninUsername.Name = "txtSigninUsername";
+            txtSigninUsername.Size = new Size(370, 42);
+            txtSigninUsername.TabIndex = 1;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Times New Roman", 15.75F, FontStyle.Italic, GraphicsUnit.Point);
+            label9.Location = new Point(28, 132);
+            label9.Name = "label9";
+            label9.Size = new Size(180, 31);
+            label9.TabIndex = 24;
+            label9.Text = "Phone Number";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Times New Roman", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label10.ForeColor = Color.Maroon;
+            label10.Location = new Point(160, 77);
+            label10.Name = "label10";
+            label10.Size = new Size(109, 39);
+            label10.TabIndex = 19;
+            label10.Text = "Signin";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Times New Roman", 27.75F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label11.ForeColor = Color.Maroon;
+            label11.Location = new Point(53, 20);
+            label11.Name = "label11";
+            label11.Size = new Size(333, 52);
+            label11.TabIndex = 20;
+            label11.Text = "Client Chat-app";
+            // 
+            // txtSigninPhoneNumber
+            // 
+            txtSigninPhoneNumber.Font = new Font("Times New Roman", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            txtSigninPhoneNumber.Location = new Point(28, 172);
+            txtSigninPhoneNumber.Margin = new Padding(3, 4, 3, 4);
+            txtSigninPhoneNumber.Name = "txtSigninPhoneNumber";
+            txtSigninPhoneNumber.Size = new Size(370, 42);
+            txtSigninPhoneNumber.TabIndex = 25;
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Font = new Font("Times New Roman", 15.75F, FontStyle.Italic, GraphicsUnit.Point);
+            label12.Location = new Point(28, 439);
+            label12.Name = "label12";
+            label12.Size = new Size(281, 31);
+            label12.TabIndex = 29;
+            label12.Text = "Password Confirmation";
+            // 
+            // txtSigninPasswordConfirm
+            // 
+            txtSigninPasswordConfirm.Font = new Font("Times New Roman", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            txtSigninPasswordConfirm.Location = new Point(28, 479);
+            txtSigninPasswordConfirm.Margin = new Padding(3, 4, 3, 4);
+            txtSigninPasswordConfirm.Name = "txtSigninPasswordConfirm";
+            txtSigninPasswordConfirm.Size = new Size(370, 42);
+            txtSigninPasswordConfirm.TabIndex = 26;
+            txtSigninPasswordConfirm.UseSystemPasswordChar = true;
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Font = new Font("Times New Roman", 15.75F, FontStyle.Italic, GraphicsUnit.Point);
+            label13.Location = new Point(28, 439);
+            label13.Name = "label13";
+            label13.Size = new Size(121, 31);
+            label13.TabIndex = 28;
+            label13.Text = "Password";
+            // 
+            // textBox4
+            // 
+            textBox4.Enabled = false;
+            textBox4.Font = new Font("Times New Roman", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox4.Location = new Point(28, 479);
+            textBox4.Margin = new Padding(3, 4, 3, 4);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(370, 42);
+            textBox4.TabIndex = 27;
+            textBox4.UseSystemPasswordChar = true;
+            // 
+            // Signin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(432, 698);
+            Controls.Add(label12);
+            Controls.Add(txtSigninPasswordConfirm);
+            Controls.Add(label13);
+            Controls.Add(textBox4);
+            Controls.Add(txtSigninPhoneNumber);
+            Controls.Add(btnSignin);
+            Controls.Add(lblSignin);
+            Controls.Add(label7);
+            Controls.Add(txtSigninPassword);
+            Controls.Add(label8);
+            Controls.Add(txtSigninUsername);
+            Controls.Add(label9);
+            Controls.Add(label10);
+            Controls.Add(label11);
+            Controls.Add(btnStart);
+            Controls.Add(label4);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label6);
+            Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Signin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Signin";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnStart;
+        private Label label4;
+        private TextBox textBox2;
+        private Label label3;
+        private TextBox textBox1;
+        private Label label6;
+        private Label label1;
+        private Button btnSignin;
+        private Label lblSignin;
+        private Label label7;
+        private TextBox txtSigninPassword;
+        private Label label8;
+        private TextBox txtSigninUsername;
+        private Label label9;
+        private Label label10;
+        private Label label11;
+        private TextBox txtSigninPhoneNumber;
+        private Label label12;
+        private TextBox txtSigninPasswordConfirm;
+        private Label label13;
+        private TextBox textBox4;
+    }
+}
---
+++ Chat-app Server/Server.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Chat_app_Server
+{
+    partial class Server
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            btnStart = new Button();
+            rtbDialog = new RichTextBox();
+            btnStop = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 20.25F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label1.ForeColor = Color.Maroon;
+            label1.Location = new Point(24, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(246, 39);
+            label1.TabIndex = 0;
+            label1.Text = "Server Chat-app";
+            // 
+            // btnStart
+            // 
+            btnStart.BackColor = Color.RosyBrown;
+            btnStart.FlatStyle = FlatStyle.Flat;
+            btnStart.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnStart.ForeColor = Color.Maroon;
+            btnStart.Location = new Point(12, 81);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(306, 32);
+            btnStart.TabIndex = 0;
+            btnStart.Text = "Start";
+            btnStart.UseVisualStyleBackColor = false;
+            btnStart.Click += btnStart_Click;
+            // 
+            // rtbDialog
+            // 
+            rtbDialog.Enabled = false;
+            rtbDialog.Location = new Point(12, 141);
+            rtbDialog.Name = "rtbDialog";
+            rtbDialog.Size = new Size(306, 248);
+            rtbDialog.TabIndex = 4;
+            rtbDialog.Text = "";
+            // 
+            // btnStop
+            // 
+            btnStop.BackColor = Color.Firebrick;
+            btnStop.FlatStyle = FlatStyle.Flat;
+            btnStop.Font = new Font("Times New Roman", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnStop.ForeColor = Color.White;
+            btnStop.Location = new Point(12, 406);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(306, 32);
+            btnStop.TabIndex = 3;
+            btnStop.Text = "Stop";
+            btnStop.UseVisualStyleBackColor = false;
+            btnStop.Click += btnStop_Click;
+            // 
+            // Server
+            // 
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(361, 533);
+            Controls.Add(rtbDialog);
+            Controls.Add(btnStop);
+            Controls.Add(btnStart);
+            Controls.Add(label1);
+            Name = "Server";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Server";
+            Load += Server_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Button btnStart;
+        private RichTextBox rtbDialog;
+        private Button btnStop;
+    }
+}
